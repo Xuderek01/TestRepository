4488bb1c7178c053313bf49ef870cf2e2b33b4d5
Xuderek01 <epnightmare02@gmail.com>
2021-04-07 17:56:12 -0400

    Added ifMark

---
+++ ifMark.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ifMark {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N1 = sc.nextInt();
+        int N2 = sc.nextInt();
+
+        for (int i = 1; i <= N1; i++){
+            
+            if(N1%i == 0 && N2%i == 0){
+                System.out.println(i + " is a factor of " + N1 + " and " + N2);
+            }
+            
+        }
+
+    }
+    
+}
